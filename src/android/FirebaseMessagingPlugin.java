c5fc3568e9da5b233d9b626fadcfb1ea0e9c4999
CodbyMauro <86434506+CodbyMauro@users.noreply.github.com>
2024-10-09 15:10:11 -0300

    Update FirebaseMessagingPlugin.java

--- src/android/FirebaseMessagingPlugin.java
+++ src/android/FirebaseMessagingPlugin.java
@@ -7,6 +7,7 @@ import android.Manifest;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
+import android.app.PendingIntent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.net.Uri;
